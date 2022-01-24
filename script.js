043e9237beb2c4f409ec9f56b7810f06ad3a5b21
perlaagomezz <gomezperla879@gmail.com>
2022-01-24 09:30:11 -0800

    add

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+console.log('hello world')
+
+let random = 5; //let locally scopped
+var num = 2; //var locally scopped
+const number = 7; //const globally scopped
+
+console.log(randomNun);
+console.log(num);
+console.log(number);
